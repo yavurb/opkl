b0bfb6121cbfc6cffeefdbd915aa4024ebb38694
roy <yavurb@gmail.com>
2025-04-19 13:32:26 -0400

    :sparkles: feat(options): add the ability to use a custom OP client

--- options.go
+++ options.go
@@ -1,9 +1,19 @@
 package opkl
 
+import "github.com/1password/onepassword-sdk-go"
+
 // Configures the 1Password token used when reading a secret
 func WithToken(token string) option {
 	return func(opkl *opklReader) error {
 		opkl.opToken = token
+
+		return nil
+	}
+}
+
+func WithClient(client *onepassword.Client) option {
+	return func(opkl *opklReader) error {
+		opkl.opClient = client
 
 		return nil
 	}
